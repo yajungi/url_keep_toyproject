d881ed666deab71ba23d9fb8d813e3ac57021a6e
kimjungi <yajungi@hanmail.net>
2020-03-16 19:30:40 +0900

    Modified naming
    class name 변경

--- src/main/java/com/jungi/toy/link/controller/LinkWriteController.java
+++ src/main/java/com/jungi/toy/link/controller/LinkWriteController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LinksWriteController {
+public class LinkWriteController {
     private static final String LINK_WRITE_VIEW = "LinkWriteView";
 
     @GetMapping("/user/links")
